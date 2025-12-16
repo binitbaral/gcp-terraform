c2b30c4497093e7251c8ed074ba4f78808a0e85b
Binit Baral <binit77x@gmail.com>
2025-12-16 17:35:08 +0545

    code 17

--- service-account.tf
+++ service-account.tf
@@ -1,5 +1,5 @@
 resource "google_service_account" "infra_automation_sa" {
-account_id = "infra-automation-sa"
-display_name = "Terraform Infra Automation Service Account"
-project = "hopeful-list-446012-q4"
+  account_id   = "infra-automation-sa"
+  display_name = "Terraform Infra Automation Service Account"
+  project      = "hopeful-list-446012-q4"
 }
--- variables.tf
+++ variables.tf
@@ -1,30 +1,30 @@
 variable "project_id" {
-description = "GCP Project ID"
-type = string
+  description = "GCP Project ID"
+  type        = string
 }
 
 
 variable "project_number" {
-description = "GCP Project Number"
-type = string
+  description = "GCP Project Number"
+  type        = string
 }
 
 
 variable "region" {
-description = "Default region"
-type = string
-default = "us-central1"
+  description = "Default region"
+  type        = string
+  default     = "us-central1"
 }
 
 
 variable "github_repo" {
-description = "GitHub repository in owner/repo format"
-type = string
-default = "binit77x/gcp-terraform"
+  description = "GitHub repository in owner/repo format"
+  type        = string
+  default     = "binit77x/gcp-terraform"
 }
 
 
 variable "bucket_name" {
-description = "Cloud Storage bucket name"
-type = string
+  description = "Cloud Storage bucket name"
+  type        = string
 }
