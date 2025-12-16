60f872f77b6b1cb2d55273ccac466ab4879f70ef
Binit Baral <binit77x@gmail.com>
2025-12-16 16:20:49 +0545

    code 14

---
+++ provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "hopeful-list-446012-q4"
+  region  = "us-central1"
+}
